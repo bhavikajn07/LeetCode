9f3ec2c55034e979fc67554dc4a71cc4f83c0ded
bhavikajn07 <bhavika.jn07@gmail.com>
2026-09-14 16:01:36 +0530

    Time: 11 ms (37.07%), Space: 19.3 MB (19.22%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,20 +1,9 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        list_x = list(str(x))
-        total = len(list_x)
-        mid = total // 2
-        
         if x < 0:
             return False
-            
-        left = list_x[:mid]
-        
-        if total % 2 != 0:
-            right = list_x[mid + 1:]
-        else:
-            right = list_x[mid:]
-        
-        if left == right[::-1]:
+        elif x == 0:
             return True
-        else: 
-            return False
+        else:
+             rev_num = int(str(x)[::-1])
+        return rev_num == x
